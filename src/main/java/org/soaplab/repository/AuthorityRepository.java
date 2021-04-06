394f196370434c0474dabfc90ab5235d5d1a0932
Markus Block <markusblock@Markuss-MBP.fritz.box>
2021-04-06 08:13:37 +0200

    Initial version of soaplab generated by JHipster-7.0.0

---
+++ src/main/java/org/soaplab/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package org.soaplab.repository;
+
+import org.soaplab.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/org/soaplab/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package org.soaplab.web.rest;
